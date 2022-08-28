0a8f39cf6a3f33201fdb37d5671f34e83696cdc8
Sebastiao Gazolla Costa Junior <gazolla@gmail.com>
2022-08-28 14:49:41 -0300

    Initial Commit

---
+++ CurrencyConversion/CurrencyConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConversionApp.swift
+//  CurrencyConversion
+//
+//  Created by sebastiao Gazolla Costa Junior on 28/08/22.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
