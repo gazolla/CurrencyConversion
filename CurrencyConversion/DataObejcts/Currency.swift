cc35b6f918def9801371b8f361da9605e1e37a0c
Sebastiao Gazolla Costa Junior <gazolla@gmail.com>
2022-08-28 15:11:09 -0300

    add Currency files.

---
+++ CurrencyConversion/DataObejcts/Currency.swift
@@ -0,0 +1,24 @@
+//
+//  Currency.swift
+//  CurrencyConversion
+//
+//  Created by sebastiao Gazolla Costa Junior on 28/08/22.
+//
+
+import Foundation
+
+class Currency:Identifiable {
+    
+    var countryName:String?
+    var countryCode:String?
+    var currencyCode:String?
+    var currencyName:String?
+    var currencySymbol:String?
+    
+ }
+
+extension Currency:CustomStringConvertible {
+    var description: String {
+        return "\nCountryCode   : \(self.countryCode!)\nName         : \(self.countryName!)\nCurrencyCode : \(self.currencyCode!)\ncurrencyName: \(self.currencyName!)\ncurrencySymbol: \(self.currencySymbol!)\n----------------------------"
+    }
+}
---
+++ CurrencyConversion/DataServices/CurencyDataService.swift
@@ -0,0 +1,45 @@
+//
+//  CurencyDataService.swift
+//  CurrencyConversion
+//
+//  Created by sebastiao Gazolla Costa Junior on 28/08/22.
+//
+
+import Foundation
+
+class CurrencyDataService {
+    static let instance = CurrencyDataService()
+    
+    var currencies:[Currency] = []
+    
+    private init(){
+        loadEveryCountryWithCurrency()
+    }
+    
+    func loadEveryCountryWithCurrency() {
+    
+        let localeCurrencies = Locale.commonISOCurrencyCodes
+        for currencyCode in localeCurrencies {
+            
+            let currency = Currency()
+            currency.currencyCode = currencyCode
+            
+            let currencyLocale = Locale(identifier: currencyCode)
+            currency.currencyName = (currencyLocale as NSLocale).displayName(forKey:NSLocale.Key.currencyCode, value: currencyCode)
+            currency.countryCode = String(currencyCode.prefix(2))
+            currency.currencySymbol = (currencyLocale as NSLocale).displayName(forKey:NSLocale.Key.currencySymbol, value: currencyCode)
+
+            
+            let countryLocale  = NSLocale.current
+            currency.countryName = (countryLocale as NSLocale).displayName(forKey: NSLocale.Key.countryCode, value: currency.countryCode!)
+            
+            
+            if currency.countryName != nil {
+                self.currencies.append(currency)
+            }
+            
+        }
+    
+    }
+
+}
